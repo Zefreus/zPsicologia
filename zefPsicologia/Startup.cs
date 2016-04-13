55e6d4eabc7cd81b6d5bb5fc015413c10232b764
Tiago de Vargas Leal <tiago.leal@rcadigital.com.br>
2016-04-13 14:00:57 -0300

    tudo

---
+++ zefPsicologia/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(zefPsicologia.Startup))]
+namespace zefPsicologia
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
